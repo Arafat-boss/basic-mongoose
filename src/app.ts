2d6708be5f6be6bcf5424db4bbfa39ee3b183372
arafat <mmarafatu@gmail.com>
2025-08-07 05:43:28 +0600

    i am create Schema and Model and it's successfully working

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,29 @@
 import express, { Application, Request, Response } from 'express';
+import mongoose, { Schema } from 'mongoose';
 
 const app: Application = express()
 
-app.get('/' ,(req: Request, res: Response)=>{
+const noteSchema = new Schema({
+    title: String,
+    containt: String,
+
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+app.post('/creaate-note',(req: Request, res: Response )=>{
+    const myNote = new Note({
+        title: "Learing Mongooses",
+        containt: "Learing Mongooses in Programming hero"
+    })
+    res.status(201).json({
+        success: true,
+        message: "Note create successfully",
+        note: myNote
+    })
+})
+
+app.get('/' ,(req: Request, res: Response )=>{
     res.send('Welcome To Our Server')
 })
 
