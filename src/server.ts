d74cc53d89c67f13d8b5eb8f3f4ab24a146b2ab3
arafat <mmarafatu@gmail.com>
2025-08-07 04:36:24 +0600

    i am learning a mongoose

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import {Server} from 'http';
+import app from './app';
+import mongoose from 'mongoose';
+
+let server: Server;
+const PORT = 5000;
+
+async function main() {
+   try {
+     await mongoose.connect(`mongodb+srv://noteApp:ME0xoQzMSUwbJICm@cluster0.j4yvqpr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+     console.log("****connected to Mongoose*****");
+        server = app.listen(PORT, ()=>{
+            console.log(`App is listening on port ${PORT}`);
+        })
+   } catch (error) {
+        console.log(error);
+   }
+}
+
+main()
