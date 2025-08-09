d42cd8369e28bedb7c72a08bff5fa0550082ea2a
arafat <mmarafatu@gmail.com>
2025-08-10 04:41:30 +0600

    now i am create new collection Name (user), and using typescript in mongoose(with mongoose doc)

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import express, { Application, Request, Response } from "express";
 import mongoose, { Schema } from "mongoose";
 import { Note } from "./models/note.models";
 import { notesRoutes } from "./controllers/notes.controller";
+import { userRoutes } from "./controllers/users.controller";
 
 const app: Application = express();
 
 app.use(express.json())
 
 app.use("/notes", notesRoutes)
-
+app.use("/users", userRoutes)
 
 
 
---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,66 @@
+ import express, { Request, Response } from"express"
+
+import { User } from "../models/user.models";
+
+
+export const userRoutes = express.Router()
+
+// ================note create===============
+userRoutes.post("/create-user", async (req: Request, res: Response) => {
+
+//approach 02 to data inchart mongodb
+const body = req.body;
+const user = await User.create(body)
+
+  res.status(201).json({
+    success: true,
+    message: "User create successfully",
+    user
+  });
+});
+//==================gate all note=============
+userRoutes.get("/", async (req: Request, res: Response) => {
+  const notes = await User.find();
+  console.log(notes);
+  res.status(200).json({
+    success: true,
+    notes,
+  });
+});
+//==================gate single note=============
+userRoutes.get("/:userId", async (req: Request, res: Response) => {
+        const userId = req.params.userId;
+        const users = await User.findById(userId)
+  console.log(users);
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+//==================Update========================
+userRoutes.patch("/:userId", async (req: Request, res: Response) => {
+    const updatedBody = req.body;
+    const userId = req.params.userId;
+    console.log(userId);
+    
+    console.log("updatebody",updatedBody);
+    const users = await User.findByIdAndUpdate(userId, updatedBody,{new: true})
+       // const note = await Note.findOneAndUpdate({ _id: noteId }, updatedBody, { new: true, })
+       // const note = await Note.updateOne({ _id: noteId }, updatedBody, { new: true, })
+  console.log(users);
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+//==================Delete========================
+userRoutes.delete("/:userId", async (req: Request, res: Response) => {
+    const userId = req.params.noteId;
+    const users = await User.findByIdAndDelete(userId)
+    // const note1 = await Note.findOneAndDelete({ _id: noteId })
+    // const note2 = await Note.deleteOne({ _id: noteId })
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
