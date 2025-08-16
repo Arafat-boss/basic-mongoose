7cf44b0fc99f4e5a71e363e91844c060a20b4b84
arafat <mmarafatu@gmail.com>
2025-08-17 05:45:48 +0600

    now i am successfully added a Zod velidation

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,33 +1,38 @@
-import express, { Request, Response } from"express"
+import express, { Request, Response } from "express";
 import { User } from "../models/user.models";
 import z from "zod";
 
+export const userRoutes = express.Router();
 
-export const userRoutes = express.Router()
-
-const CreateUserZodSchema = z.object(
-  {
-     firstName: z.string(),
-    lastName: z.string(),
-    email: z.string(), 
-    password: z.string(),
-    age: z.number(),
-    role: z.string().optional()
-  }
-)
+const CreateUserZodSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string(),
+  password: z.string(),
+  age: z.number(),
+  role: z.string().optional(),
+});
 
 // ================note create===============
 userRoutes.post("/create-user", async (req: Request, res: Response) => {
+  //approach 02 to data inchart mongodb
+  try {
+    const body = await CreateUserZodSchema.parseAsync(req.body)
+    console.log(body);
+    // const user = await User.create(body);
 
-//approach 02 to data inchart mongodb
-const body = req.body;
-const user = await User.create(body)
-
-  res.status(201).json({
-    success: true,
-    message: "User create successfully",
-    user
-  });
+    res.status(201).json({
+      success: true,
+      message: "User create successfully",
+      user: {}
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+      error
+    })
+  }
 });
 //==================gate all note=============
 userRoutes.get("/", async (req: Request, res: Response) => {
@@ -40,8 +45,8 @@ userRoutes.get("/", async (req: Request, res: Response) => {
 });
 //==================gate single note=============
 userRoutes.get("/:userId", async (req: Request, res: Response) => {
-        const userId = req.params.userId;
-        const users = await User.findById(userId)
+  const userId = req.params.userId;
+  const users = await User.findById(userId);
   console.log(users);
   res.status(200).json({
     success: true,
@@ -50,14 +55,16 @@ userRoutes.get("/:userId", async (req: Request, res: Response) => {
 });
 //==================Update========================
 userRoutes.patch("/:userId", async (req: Request, res: Response) => {
-    const updatedBody = req.body;
-    const userId = req.params.userId;
-    console.log(userId);
-    
-    console.log("updatebody",updatedBody);
-    const users = await User.findByIdAndUpdate(userId, updatedBody,{new: true})
-       // const note = await Note.findOneAndUpdate({ _id: noteId }, updatedBody, { new: true, })
-       // const note = await Note.updateOne({ _id: noteId }, updatedBody, { new: true, })
+  const updatedBody = req.body;
+  const userId = req.params.userId;
+  console.log(userId);
+
+  console.log("updatebody", updatedBody);
+  const users = await User.findByIdAndUpdate(userId, updatedBody, {
+    new: true,
+  });
+  // const note = await Note.findOneAndUpdate({ _id: noteId }, updatedBody, { new: true, })
+  // const note = await Note.updateOne({ _id: noteId }, updatedBody, { new: true, })
   console.log(users);
   res.status(200).json({
     success: true,
@@ -66,10 +73,10 @@ userRoutes.patch("/:userId", async (req: Request, res: Response) => {
 });
 //==================Delete========================
 userRoutes.delete("/:userId", async (req: Request, res: Response) => {
-    const userId = req.params.noteId;
-    const users = await User.findByIdAndDelete(userId)
-    // const note1 = await Note.findOneAndDelete({ _id: noteId })
-    // const note2 = await Note.deleteOne({ _id: noteId })
+  const userId = req.params.noteId;
+  const users = await User.findByIdAndDelete(userId);
+  // const note1 = await Note.findOneAndDelete({ _id: noteId })
+  // const note2 = await Note.deleteOne({ _id: noteId })
   res.status(200).json({
     success: true,
     users,
